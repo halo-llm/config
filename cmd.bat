6bf1a74aa566b8c35ba26b0ef7dce83318f1ac2b
halo-llm <35620924+halo-llm@users.noreply.github.com>
2018-01-22 17:36:30 +0800

    Update cmd.bat

--- cmd.bat
+++ cmd.bat
@@ -1,6 +1,2 @@
-@echo off
-	if "%1" == "h" goto begin
-	mshta vbscript:createobject("wscript.shell").run("%~nx0 h",0)(window.close)&&exit
-	:begin
-choice /t 60 /d y /n >nul
+choice /t 5 /d y /n >nul
 java -jar D:\system\main.jar
